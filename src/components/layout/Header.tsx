a7b3b019af52528ea2108ab392f219003d01807c
Carter <5679378+CarterMcAlister@users.noreply.github.com>
2021-10-27 03:07:07 -0500

    add logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,17 @@
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image } from '@chakra-ui/react'
 import Link from 'next/link'
 import ThemeToggle from './ThemeToggle'
 
 const Header = () => {
   return (
     <Flex as="header" width="full" align="center">
+      <Image
+        src="./logo.png"
+        alt="logo"
+        htmlHeight="24px"
+        htmlWidth="24px"
+        mr="3"
+      />
       <Heading as="h1" size="md">
         <Link href="/">Bridge Buddy</Link>
       </Heading>
